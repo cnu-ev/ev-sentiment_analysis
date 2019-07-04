42dafa00f4475b844bc46275b6df18232925d98d
lsh23 <shseoul14@gmail.com>
2019-07-04 17:03:45 +0900

    add main func

--- preprocessing.py
+++ preprocessing.py
@@ -10,3 +10,19 @@ def read_data(file):
 
 def tokenize(text):
     return ['/'.join(token) for token in tagger.pos(text)]
+
+
+if __name__ == "__main__":
+
+train_data = read_data('./data_set/ratings_train.txt')
+test_data = read_data('./data_set/ratings_test.txt')
+
+tagger = Mecab()
+train_text = [(tokenize(row[1]),row[2]) for row in train_data]
+test_text = [(tokenize(row[1]),row[2]) for row in test_data]
+
+with open('./data_set_to_pickle/train_text_set.pkl', 'wb') as f:
+    pickle.dump(train_text, f)
+
+with open('./data_set_to_pickle/test_text.pkl', 'wb') as f:
+    pickle.dump(test_text, f)
