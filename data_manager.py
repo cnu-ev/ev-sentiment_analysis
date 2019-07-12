9f0d6cd9acb05e6dccfc98042772c4b80b16a75a
lsh23 <shseoul14@gmail.com>
2019-07-12 16:31:44 +0900

    edit typo

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,11 @@
 import pickle
 train_text_dir = './data_set_to_pickle/train_text_set.pkl''
-train_text_dir = './data_set_to_pickle/test_text.pkl'
+test_text_dir = './data_set_to_pickle/test_text.pkl'
 class data_manager:
     def __init__(self,train_text_dir,train_text_dir):
         with open(train_text_dir, 'rb') as f:
             self.train_text = pickle.load(f)
-        with open(train_text_dir, 'rb') as f:
+        with open(test_text_dir, 'rb') as f:
             self.test_text = pickle.load(f)
 
     def get_train_sentences(self):
