3804aa7e05aff099827d371f1ae52b9f0b6da5d7
lsh23 <shseoul14@gmail.com>
2019-07-15 15:50:44 +0900

    add sentiment_analysis_model.py

---
+++ sentiment_analysis_model.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+
+import word2vec_embedder
+import data_preprocessor
+
+class Sentiment_analysis_model:
+    def __init__(self, trainDataVecs , train_tags):
+        self.x_train = trainDataVecs
+        self.y_train = np.asarray(train_tags).astype('float32')
+
+
+# print(len(x_train))
+
+
+# len(y_train)
+    def make_model(self):
+        run_model = models.Sequential()
+        run_model.add(layers.Dense(64, activation='relu', input_shape=(300,)))
+        run_model.add(layers.Dense(64, activation='relu'))
+        run_model.add(layers.Dense(1, activation='sigmoid'))
+
+        run_model.compile(optimizer=optimizers.RMSprop(lr=0.002),
+                 loss=losses.binary_crossentropy,
+                 metrics=[metrics.binary_accuracy])
+
+        run_model.fit(x_train, y_train, epochs=10, batch_size=512)
