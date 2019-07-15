51b3d6b583196614b51da460276ac3f20ddbb2da
lsh23 <shseoul14@gmail.com>
2019-07-15 15:56:10 +0900

    add main script

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -12,3 +12,17 @@ class Data_preprocessor:
             data = [line.split('\t') for line in f.read().splitlines()]
             data = data[1:]
         return data
+
+if __name__ == "__main__":
+
+    train_data = read_data('./data_set/ratings_train.txt')
+    test_data = read_data('./data_set/ratings_test.txt')
+
+    train_text = [(tokenize(row[1]),row[2]) for row in train_data]
+    test_text = [(tokenize(row[1]),row[2]) for row in test_data]
+
+    with open('./data_set_to_pickle/train_text_set.pkl', 'wb') as f:
+        pickle.dump(train_text, f)
+
+    with open('./data_set_to_pickle/test_text.pkl', 'wb') as f:
+        pickle.dump(test_text, f)
