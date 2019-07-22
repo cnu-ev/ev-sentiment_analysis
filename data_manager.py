c26d88ba8cee9239aa9eec519dead3e252506f8d
lsh23 <shseoul14@gmail.com>
2019-07-22 17:20:04 +0900

    add update_data_set_from_report_data using data_preprocessor

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import pickle
+import data_preprocessor
 # train_text_dir = './data_set_to_pickle/train_text_set.pkl'
 # test_text_dir = './data_set_to_pickle/test_text.pkl'
 
@@ -18,3 +19,9 @@ class Data_manager:
         return [ sentence for sentence, tag in self.test_text]
     def get_test_tags(self):
         return [ tag for sentence, tag in self.test_text]
+
+
+    def update_data_set_from_report_data(self,report_data):
+        new_data = data_preprocessor.read_data(report_data)
+        new_train_data_set = [(data_preprocessor.tokenize(row[1]),row[2]) for row in train_data]
+        self.train_text = self.train_text + new_train_data_set
