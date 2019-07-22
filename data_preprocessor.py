1d85fe8c30e012de0c70580a397d4e9cf2c1bcc8
lsh23 <shseoul14@gmail.com>
2019-07-22 17:11:12 +0900

    edit Data_preprocessor class to only module

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -6,12 +6,12 @@ tagger = Mecab()
 def tokenize(text):
     return ['/'.join(token) for token in tagger.pos(text)]
 
-class Data_preprocessor:
-    def read_data(file):
-        with open(file,'r',encoding='utf-8') as f:
-            data = [line.split('\t') for line in f.read().splitlines()]
-            data = data[1:]
-        return data
+
+def read_data(file):
+    with open(file,'r',encoding='utf-8') as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+        data = data[1:]
+    return data
 
 if __name__ == "__main__":
 
