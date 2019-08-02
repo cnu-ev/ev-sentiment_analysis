b593b12faad9123c90188d510b9a92afb823e0c1
lsh23 <shseoul14@gmail.com>
2019-08-02 15:44:31 +0900

    edit to making model  to loading model

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from data_manager import Data_manager
 from word2vec_embedder import Word2vec_embedder
 from sentiment_analysis_model import Sentiment_analysis_model
 import word2vec_embedder as word2vec_embbeding_module
+import pickle
 
 class Sentiment_analysis:
     def __init__(self):
-        #전처리 완료된 학습데이터 경로
-
         #data_manager 생성
         self.data_manager = Data_manager()
 
@@ -18,16 +17,20 @@ class Sentiment_analysis:
         self.word2vec_embedder = Word2vec_embedder(train_sentences)
 
         #word2vec 방식으로 embedding 된 vector_set
-        trainDataVecs = word2vec_embbeding_module.getAvgFeatureVecs(train_sentences , self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+        with open('trainDataVecs.pkl', 'rb') as f:
+            trainDataVecs = pickle.load(f)
 
         #embedding 된 vector_set과 정답지로 Sentiment_analysis 모델 생성
         self.sentiment_analysis_model = Sentiment_analysis_model(trainDataVecs,train_tags)
 
     def Sentiment_analysis_predict_pos_neg(self,sentence):
-        sentence, score = self.sentiment_analysis_model.predict_pos_neg_by_loaded_model(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+        # sentence, score = self.sentiment_analysis_model.predict_pos_neg_by_loaded_model(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+        sentence, score = self.sentiment_analysis_model.predict_pos_neg(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+
         return sentence,score
+
 
 if __name__ == "__main__":
     #전처리 완료된 학습데이터 경로
     sentiment_analysis = Sentiment_analysis()
-    sentence, score = sentiment_analysis.Sentiment_analysis_predict_pos_neg("잘되네요 좋습니다.")
+    sentiment_analysis.Sentiment_analysis_predict_pos_neg("테스트")
