e63dac7078c2117aee0ef20e74447ac5fa1b9d83
lsh23 <shseoul14@gmail.com>
2019-08-02 15:46:00 +0900

    add update_model function

--- main.py
+++ main.py
@@ -29,8 +29,26 @@ class Sentiment_analysis:
 
         return sentence,score
 
+    def update_model(self):
+        #data_manager 생성
+        self.data_manager = Data_manager()
+        self.data_manager.update_train_data_set_from_report_data('report.txt')
+        train_sentences = self.data_manager.get_train_sentences()
+        train_tags = self.data_manager.get_train_tags()
+        print(len(train_sentences))
+        print(len(train_tags))
+
+        # 학습 문장으로 word2_vec_embedder 생성
+        self.word2vec_embedder.make_model(train_sentences)
+        #word2vec 방식으로 embedding 된 vector_set
+        trainDataVecs = word2vec_embbeding_module.getAvgFeatureVecs(train_sentences , self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+        with open('trainDataVecs.pkl','wb') as f:
+            pickle.dump(trainDataVecs, f)
+        #embedding 된 vector_set과 정답지로 Sentiment_analysis 모델 생성
+        self.sentiment_analysis_model = Sentiment_analysis_model(trainDataVecs,train_tags)
+
 
 if __name__ == "__main__":
-    #전처리 완료된 학습데이터 경로
+
     sentiment_analysis = Sentiment_analysis()
-    sentiment_analysis.Sentiment_analysis_predict_pos_neg("테스트")
+    sentiment_analysis.Sentiment_analysis_predict_pos_neg("")
