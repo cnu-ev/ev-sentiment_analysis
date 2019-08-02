7aba45d27eadc18ed3e4ad3f3a1104530f82f2ec
lsh23 <shseoul14@gmail.com>
2019-08-02 16:34:44 +0900

    edit to making model to loading model

--- sentiment_analysis_model.py
+++ sentiment_analysis_model.py
@@ -13,7 +13,7 @@ class Sentiment_analysis_model:
     def __init__(self, trainDataVecs , train_tags):
         self.x_train = trainDataVecs
         self.y_train = np.asarray(train_tags).astype('float32')
-        self.make_model()
+        self.run_model = self.load_keras_model()
 
 # print(len(x_train))
 
@@ -29,8 +29,9 @@ class Sentiment_analysis_model:
                  loss=losses.binary_crossentropy,
                  metrics=[metrics.binary_accuracy])
 
-        self.run_model.fit(self.x_train, self.y_train, epochs=10, batch_size=512)
+        self.run_model.fit(self.x_train, self.y_train, epochs=20, batch_size=512)
         self.save_model()
+
 
     def model_evaluate(self,testDataVecs,test_tags):
         x_test = testDataVecs
@@ -61,5 +62,7 @@ class Sentiment_analysis_model:
     def load_keras_model(self):
         model = load_model('model.h5')
         return model
+
+
 
     # predict_pos_neg("아 이규봉이 디자인한 댓글페이지 좋은데요? 추천합니다 사용할께요",run_model)
