515bb259de4abea849a2250153b868d30e5b60d6
lsh23 <shseoul14@gmail.com>
2019-08-13 18:58:26 +0900

    edit for pytorch_ver seperate text, label

--- data_manager.py
+++ data_manager.py
@@ -3,26 +3,34 @@ import data_preprocessor
 
 
 class Data_manager:
-    preprocessed_train_text_dir = './data_set_to_pickle/preprocessed_text_train.pkl'
-    preprocessed_test_text_dir = './data_set_to_pickle/preprocessed_text_test.pkl'
+    generalized_train_data_tagged_dir = './data_set_to_pickle/generalized_train_data_tagged.pkl'
+    generalized_test_data_tagged_dir = './data_set_to_pickle/generalized_test_data_tagged.pkl'
+    train_label_dir = 'data_set_to_pickle/train_label.pkl'
+    test_label_dir = 'data_set_to_pickle/test_label.pkl'
+
     def __init__(self):
-        self.train_text = self.load_data(self.preprocessed_train_text_dir)
-        self.test_text = self.load_data(self.preprocessed_test_text_dir)
+        self.train_text = self.load_data(self.generalized_train_data_tagged_dir)
+        self.test_text = self.load_data(self.generalized_test_data_tagged_dir)
+        self.train_label = self.load_data(self.train_label_dir)
+        self.test_label = self.load_data(self.test_label_dir)
 
     def get_train_sentences(self):
-        return [ sentence for sentence, tag in self.train_text]
+        return self.train_text
     def get_train_tags(self):
-        return [ tag for sentence, tag in self.train_text]
+        return self.train_label
 
     def get_test_sentences(self):
-        return [ sentence for sentence, tag in self.test_text]
+        return self.test_text
     def get_test_tags(self):
-        return [ tag for sentence, tag in self.test_text]
+        return self.test_label
+
     #report_data_dir는 경로
     def update_train_data_set_from_report_data(self,report_data_dir):
         new_data = data_preprocessor.read_data(report_data_dir)
-        new_train_data_set = [(data_preprocessor.tokenize(row[1]),row[2]) for row in new_data]
-        self.train_text = self.train_text + new_train_data_set
+        new_train_data_text = [(data_preprocessor.texts_generalizing(row[1])) for row in new_data]
+        new_train_data_label = [ row[2] for row in new_data ]
+        self.train_text = self.train_text + new_train_data_text
+        self.train_label = self.train_label + new_train_data_label
         # save_data(self.train_text,self.preprocessed_train_text_dir)
 
     def save_data(self,data_list,data_dir):
