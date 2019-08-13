2261c133ad0fae59304d99afbb298ab94c0a189e
lsh23 <shseoul14@gmail.com>
2019-08-13 19:00:09 +0900

    edit NN to lstm model

--- sentiment_analysis_model.py
+++ sentiment_analysis_model.py
@@ -1,73 +1,141 @@
+import os
+import time
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
 import numpy as np
-
-from tensorflow.keras import models
-from tensorflow.keras import layers
-from tensorflow.keras import optimizers
-from tensorflow.keras import losses
-from tensorflow.keras import metrics
-from tensorflow.keras.models import load_model
-import word2vec_embedder
+import glove_embedder
 import data_preprocessor
+from lstm_attn import AttentionModel
+
+# testttt = torch.load('index_torch.pt')
+# testttt_test = torch.load('index_torch_test.pt')
+
 
 class Sentiment_analysis_model:
-    def __init__(self, trainDataVecs , train_tags):
-        self.x_train = trainDataVecs
-        self.y_train = np.asarray(train_tags).astype('float32')
-        self.run_model = self.load_keras_model()
-        # self.make_model()
+    # def __init__(self, trainDataVecs , train_tags):
+    #     self.x_train = trainDataVecs
+    #     self.y_train = train_tags
+    #     batch_size = 32
+    #     learning_rate = 2e-5
+    #     output_size = 2
+    #     hidden_size = 256
+    #     embedding_length = 100
+    #     self.model = AttentionModel(batch_size,output_size, hidden_size, vocab_size, embedding_length, word_embeddings)
+    #     loss_fn = F.cross_entropy
+    #     self.train_model(self.model, testttt ,train_label )
 
+        # self.make_model()
+    def __init__(self,glove_dictionary,glove_vector):
+        self.model = self.load_torch_model(glove_dictionary,glove_vector)
 # print(len(x_train))
 
+    def clip_gradient(self,clip_value):
+        params = list(filter(lambda p: p.grad is not None, self.model.parameters()))
+        for p in params:
+            p.grad.data.clamp_(-clip_value, clip_value)
 
-# len(y_train)
-    def make_model(self):
-        self.run_model = models.Sequential()
-        self.run_model.add(layers.Dense(64, activation='relu', input_shape=(300,)))
-        self.run_model.add(layers.Dense(64, activation='relu'))
-        self.run_model.add(layers.Dense(1, activation='sigmoid'))
-
-        self.run_model.compile(optimizer=optimizers.RMSprop(lr=0.002),
-                 loss=losses.binary_crossentropy,
-                 metrics=[metrics.binary_accuracy])
-
-        self.run_model.fit(self.x_train, self.y_train, epochs=10, batch_size=512)
+    def train_model(self,trainDataVecs, y_train):
+        optim = torch.optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()))
+        self.model.cuda()
+        self.model.train()
+        count = int(len(trainDataVecs)/32)
+        for i in range(count):
+            text = trainDataVecs[32*i:32*(i+1)]
+            target = y_train[32*i:32*(i+1)]
+            target = torch.autograd.Variable(target).long()
+            if torch.cuda.is_available():
+                text = text.cuda()
+                target = target.cuda()
+            optim.zero_grad()
+            prediction = self.model(text)
+            loss = loss_fn(prediction, target)
+            loss.backward()
+            clip_gradient(self.model, 1e-1)
+            optim.step()
+            if(i%40==0):
+                print(i,count)
         self.save_model()
+        # text = trainDataVecs[500*count:]
+        # target = y_train[500*count:]
+        # target = torch.autograd.Variable(target).long()
+        # optim.zero_grad()
+        # prediction = model(text)
+        # loss = loss_fn(prediction, target)
+        # loss.backward()
+        # clip_gradient(model, 1e-1)
+        # optim.step()
 
 
-    def model_evaluate(self,testDataVecs,test_tags):
-        x_test = testDataVecs
-        y_test = np.asarray(test_tags).astype('float32')
-        results = self.run_model.evaluate(x_test, y_test)
-        return results #[loss,score]
+    def eval_model(self,testDataVecs,y_test):
+        self.model.eval()
+        correct = 0
+        count = int(len(testDataVecs)/32)
+        with torch.no_grad():
+            for i in range(count):
+                text = testDataVecs[32*i:32*(i+1)]
+                target = y_test[32*i:32*(i+1)]
+                target = torch.autograd.Variable(target).long()
+                if torch.cuda.is_available():
+                    text = text.cuda()
+                    target = target.cuda()
+                prediction = self.model(text)
+                loss = loss_fn(prediction, target)
+                num_corrects = (torch.max(prediction, 1)[1].view(target.size()).data == target.data).sum()
+                correct = correct + num_corrects
+                # acc = 100.0 * num_corrects/500
+                # total_epoch_loss += loss.item()
+                # total_epoch_acc += acc.item()
 
+            print('Accuracy of the network on the test images: %d %%' % (100 * correct / 75000))
 
-    def predict_pos_neg(self,review,word2vec_model,num_features):
-        token = data_preprocessor.tokenize(review)
-        reviewFeatureVecs = np.zeros((1,num_features),dtype="float32")
-        reviewFeatureVecs[0] = review_vec = word2vec_embedder.makeFeatureVec(token, word2vec_model, num_features)
-        score = float(self.run_model.predict(reviewFeatureVecs))
-        return review, score * 100 - 50
+    def prediction_pos_neg(self,review,glove_dictionary):
+        list_=indexing_embedding(review,glove_dictionary)
+        list_ = torch.tensor(list_)
+        self.model.batch_size = 1
+        self.model.cuda()
+        text = list_
+        if torch.cuda.is_available():
+            text = text.cuda()
+        prediction = self.model(text,batch_size=1)
+        num_corrects = torch.max(prediction, 1)[1]
+        return prediction,num_corrects
 
-    def predict_pos_neg_by_loaded_model(self,review,word2vec_model,num_features):
-        token = data_preprocessor.tokenize(review)
-        reviewFeatureVecs = np.zeros((1,num_features),dtype="float32")
-        reviewFeatureVecs[0] = review_vec = word2vec_embedder.makeFeatureVec(token, word2vec_model, num_features)
-        model = self.load_keras_model()
-        score = float(model.predict(reviewFeatureVecs))
-
-        return review, score * 100 - 50
+    def prediction_pos_neg_loading_model(self,review,glove_dictionary):
+        list_=indexing_embedding(review,glove_dictionary)
+        list_ = torch.tensor(list_)
+        self.model = self.load_torch_model()
+        self.model.eval()
+        self.model.batch_size = 1
+        self.model.cuda()
+        text = list_
+        if torch.cuda.is_available():
+            text = text.cuda()
+        prediction = self.model(text,batch_size=1)
+        num_corrects = torch.max(prediction, 1)[1]
+        return prediction,num_corrects
 
     def save_model(self):
-        self.run_model.save('model.h5')
+        torch.save(self.model.state_dict(), 'torch_model_state_generalized')
+        torch.save(self.model, 'torch_model_entire_generalized')
 
-    def load_keras_model(self):
-        model = load_model('model.h5')
+    def load_torch_model(self,glove_dictionary,glove_vector):
+        vocab_size = len(glove_dictionary)
+        batch_size = 32
+        learning_rate = 2e-5
+        output_size = 2
+        hidden_size = 256
+        embedding_length = 100
+        word_embeddings = torch.from_numpy(glove_vector)
+        model = AttentionModel(batch_size,output_size, hidden_size, vocab_size, embedding_length, word_embeddings)
+        model.load_state_dict(torch.load('torch_model_state_generalized'), strict=False)
         return model
 
     def set_x_train(trainDataVecs):
         self.x_train = trainDataVecs
     def set_y_train(train_tags):
-        self.y_train
+        self.y_train = train_tags
 
 
     # predict_pos_neg("아 이규봉이 디자인한 댓글페이지 좋은데요? 추천합니다 사용할께요",run_model)
