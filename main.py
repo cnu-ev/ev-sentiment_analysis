96baea193d08cffb9d6c551c1634de2934b79116
lsh23 <shseoul14@gmail.com>
2019-08-13 19:00:32 +0900

    edit main.py for torch_ver

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from data_manager import Data_manager
 # from data_preprocessor import Data_preprocessor
-from word2vec_embedder import Word2vec_embedder
+from glove_embedder import Glove_embedder
 from sentiment_analysis_model import Sentiment_analysis_model
-import word2vec_embedder as word2vec_embbeding_module
 import pickle
 
 class Sentiment_analysis:
     def __init__(self):
+        #data_manager 생성
+        # self.data_manager = Data_manager()
+        #
+        # train_sentences = self.data_manager.get_train_sentences()
+        # train_tags = self.data_manager.get_train_tags()
+
+        #학습 문장으로 word2_vec_embedder 생성
+        self.glove_embedder = Glove_embedder()
+        self.glove_dictionary = self.glove_embedder.get_glove_dictionary()
+        self.glove_vector = self.glove_embedder.get_glove_vector()
+        #indexing_embedding한 벡터
+
+        # train_sentences= [ data_preprocessor.indexing_embedding(sentence,glove_dictionary) for sentence in train_sentences]
+        # train_sentences_torch = torch.tensor(train_sentences)
+        # train_tags_torch = torch.tensor([int(x) for x in train_tags])
+        #embedding 된 vector_set과 정답지로 Sentiment_analysis 모델 생성
+        self.sentiment_analysis_model = Sentiment_analysis_model(self.glove_dictionary,self.glove_vector)
+
+    def Sentiment_analysis_predict_pos_neg(self,sentence):
+        # sentence, score = self.sentiment_analysis_model.predict_pos_neg_by_loaded_model(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
+        sentence, score = self.sentiment_analysis_model.predict_pos_neg(sentence,self.glove_dictionary)
+
+        return sentence,score
+
+    def training_model(self):
         #data_manager 생성
         self.data_manager = Data_manager()
 
@@ -14,38 +38,16 @@ class Sentiment_analysis:
         train_tags = self.data_manager.get_train_tags()
 
         #학습 문장으로 word2_vec_embedder 생성
-        self.word2vec_embedder = Word2vec_embedder(train_sentences)
+        #glove 임베딩에 넣는 문장들은 tagger 안된 문장 넣어야됨
+        self.glove_embedder = Glove_embedder(train_sentences)
+        self.glove_dictionary = self.glove_embedder.get_glove_dictionary()
+        #indexing_embedding한 벡터
 
-        #word2vec 방식으로 embedding 된 vector_set
-        with open('trainDataVecs.pkl', 'rb') as f:
-            trainDataVecs = pickle.load(f)
-
+        train_sentences= [ data_preprocessor.indexing_embedding(sentence,glove_dictionary) for sentence in train_sentences]
+        train_sentences_torch = torch.tensor(train_sentences)
+        train_tags_torch = torch.tensor([int(x) for x in train_tags])
         #embedding 된 vector_set과 정답지로 Sentiment_analysis 모델 생성
-        self.sentiment_analysis_model = Sentiment_analysis_model(trainDataVecs,train_tags)
-
-    def Sentiment_analysis_predict_pos_neg(self,sentence):
-        # sentence, score = self.sentiment_analysis_model.predict_pos_neg_by_loaded_model(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
-        sentence, score = self.sentiment_analysis_model.predict_pos_neg(sentence,self.word2vec_embedder.model,self.word2vec_embedder.num_features)
-
-        return sentence,score
-
-    def training_model(self):
-        #data_manager 생성
-        train_sentences = self.data_manager.get_train_sentences()
-        train_tags = self.data_manager.get_train_tags()
-
-        #학습 문장으로 word2_vec_embedder 생성
-        self.word2vec_embedder.make_model(train_sentences)
-
-
-         #word2vec 방식으로 embedding 된 vector_set
-        trainDataVecs = word2vec_embbeding_module.getAvgFeatureVecs(train_sentences , self.word2vec_embedder.model,self.word2vec_embedder.num_features)
-
-        #embedding 된 vector_set과 정답지로 Sentiment_analysis 모델 생성
-        self.sentiment_analysis_model.set_x_train(trainDataVecs)
-        self.sentiment_analysis_model.set_y_train(train_tags)
-        self.sentiment_analysis_model.make_model()
-
+        self.sentiment_analysis_model = Sentiment_analysis_model(train_sentences_torch,train_tags_torch)
 
 
     def update_model(self):
